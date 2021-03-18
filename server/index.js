21a3d88c4bb09923d7b647c7d0264119ea3eed8a
AyoungWon <thwn4750@gmail.com>
2021-03-18 21:39:55 +0900

    🧹code clear

--- server/index.js
+++ server/index.js
@@ -2,18 +2,10 @@ const express = require("express");
 const cors = require('cors')
 const app = express();
 const path = require("path");
-
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
 const config = require("./config/key");
 const cors_origin = `https://pop-movie.netlify.app`;
-// const mongoose = require("mongoose");
-// mongoose
-//   .connect(config.mongoURI, { useNewUrlParser: true })
-//   .then(() => console.log("DB connected"))
-//   .catch(err => console.error(err));
-
 const mongoose = require("mongoose");
 const connect = mongoose.connect(config.mongoURI,
   {
@@ -45,10 +37,6 @@ app.get('/', (req, res) => res.send('Hello world!!!'))
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/favorite', require('./routes/favorite'));
-
-
-//use this to show the image you have in node js server to client (react js)
-//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
